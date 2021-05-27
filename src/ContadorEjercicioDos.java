b010f89207c887267d94b778284f5701c313c206
Henry Dario Moreno Espitia <hmoreno@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-05-27 14:20:00 -0500

    adding test and code of constructor with parameter

--- src/ContadorEjercicioDos.java
+++ src/ContadorEjercicioDos.java
@@ -2,6 +2,17 @@ public class ContadorEjercicioDos {
 
     private static int counter = 0 ; // by default
 
+    public ContadorEjercicioDos(){
+        counter = 0;
+    }
 
+    public ContadorEjercicioDos(int initialValue){
+        this.counter = initialValue;
+    }
 
+    // Setters and Getters
+
+    public int getValue() {
+        return this.counter;
+    }
 }
--- src/testContadorEjercicioDos.java
+++ src/testContadorEjercicioDos.java
@@ -1,9 +1,23 @@
+import java.util.function.DoubleToIntFunction;
+
 public class testContadorEjercicioDos {
     public static void main(String[] args) {
         //test constructor por defecto
-        System.out.println("1) Test: constructor por defecto");
+        System.out.println("1) Test: constructor por defecto = 0");
         ContadorEjercicioDos counter = new ContadorEjercicioDos();
-        
+        if(counter.getValue()==0){
+            System.out.println("--- OK ----");
+        }else {
+            System.out.println("--- Red !!--");
+        }
+        System.out.println("2) Test: constructor con parametros: valor inicial .. 3");
+        ContadorEjercicioDos counterDos = new ContadorEjercicioDos(3);
+        if(counterDos.getValue() == 3){
+            System.out.println("--- OK ----");
+        }else{
+            System.out.println("--- Red !! ---");
+        }
+
 
         }
     }
