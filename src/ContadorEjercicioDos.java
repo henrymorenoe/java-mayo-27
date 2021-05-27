dafa6cd4914e12167e6470e2b23cbb5d742aad1c
Henry Dario Moreno Espitia <hmoreno@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-05-27 14:05:11 -0500

    adding class: CounterEjercicio2 and its test; proof of default constructor

---
+++ src/ContadorEjercicioDos.java
@@ -0,0 +1,7 @@
+public class ContadorEjercicioDos {
+
+    private static int counter = 0 ; // by default
+
+
+
+}
---
+++ src/testContadorEjercicioDos.java
@@ -0,0 +1,10 @@
+public class testContadorEjercicioDos {
+    public static void main(String[] args) {
+        //test constructor por defecto
+        System.out.println("1) Test: constructor por defecto");
+        ContadorEjercicioDos counter = new ContadorEjercicioDos();
+        
+
+        }
+    }
+
